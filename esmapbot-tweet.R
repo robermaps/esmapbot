dbac81a31120747aab3085bdbe048172edafa526
Rober J <programapa.contacta@gmail.com>
2021-10-12 08:57:55 +0200

    Update esmapbot-tweet.R

--- esmapbot-tweet.R
+++ esmapbot-tweet.R
@@ -10,8 +10,8 @@ esmapbot_token <- rtweet::create_token(
 # Generate random coordinates within specific limits
 lon <- round(runif(1, -9.29, 4.32), 4)
 lon <- format(lon, scientific = FALSE)
-lat <- round(runif(1, 43.79, 36.00), 4)
-lat <- format(lat, scientific = FALSE)
+lat <- round(runif(1, 36.00, 43.79), 4)
+
 
 # Build URL and fetch image from Mapbox API
 img_url <- paste0(
